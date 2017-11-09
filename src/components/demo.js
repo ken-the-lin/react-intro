958e0ce704e6b11159df28c74331ff561fc972e1
Ken Lin <xil307@ucsd.edu>
2017-11-09 10:00:38 -0800

    onclick

--- src/components/demo.js
+++ src/components/demo.js
@@ -2,11 +2,21 @@ import React from 'react'
 import { Button } from 'react-bootstrap'
 
 class Demo extends React.Component{
+    constructor(props){
+        super(props)
+        this.onClick = this.onClick.bind(this)
+    }
+
+    onClick(event){
+        event.preventDefault()
+        console.log("hey, I am clciked ")
+    }
+
     render(){
         return(
             <div>
                 hello, my name is {this.props.name}
-                <Button bsStyle="info"> click me</Button>
+                <Button bsStyle="info" onClick={this.onClick}> click me</Button>
             </div>
         )
     }
