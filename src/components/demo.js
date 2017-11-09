3124ae1ddd00843315ec7e09f06bd1a7e7645049
Ken Lin <xil307@ucsd.edu>
2017-11-09 09:57:15 -0800

    stage3: npm install pakages. use them

--- src/components/demo.js
+++ src/components/demo.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
 
 class Demo extends React.Component{
     render(){
         return(
             <div>
                 hello, my name is {this.props.name}
+                <Button bsStyle="info"> click me</Button>
             </div>
         )
     }
