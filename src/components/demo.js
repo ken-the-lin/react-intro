a4611e63c302e23d85c917d37fe7bb92493dae8a
Ken Lin <xil307@ucsd.edu>
2017-11-09 09:47:22 -0800

    demo, 1

---
+++ src/components/demo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Demo extends React.Component{
+    render(){
+        return(
+            <div>
+                hello, I am demo
+            </div>
+        )
+    }
+}
+
+export { Demo }
